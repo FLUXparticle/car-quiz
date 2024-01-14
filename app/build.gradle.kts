1de64ea3fb6dae1e3228f4098e33cdd921debb86
Sven Reinck <sreinck@fluxparticle.de>
2024-01-14 16:10:16 +0100

    Verschachtelte Build-Varianten

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,14 +45,41 @@ android {
         }
     }
 
-    flavorDimensions += "brand"
+    flavorDimensions += listOf("cluster", "brand")
 
     productFlavors {
+        create("daimler") {
+            dimension = "cluster"
+        }
+        create("bmwGroup") {
+            dimension = "cluster"
+        }
+
         create("bmw") {
             dimension = "brand"
         }
         create("mercedes") {
             dimension = "brand"
+        }
+    }
+
+    val allowedCombinations = mapOf(
+        "volkswagen" to listOf("vw", "audi", "bentley", "bugatti", "porsche", "seat", "skoda"),
+        "daimler" to listOf("maybach", "mercedes", "smart"),
+        "bmwGroup" to listOf("bmw", "mini", "rollsRoyce"),
+    )
+
+    androidComponents {
+        beforeVariants { variantBuilder ->
+            // To check for a certain build type, use variantBuilder.buildType == "<buildType>"
+            val dimensions = variantBuilder.productFlavors.associate { it }
+
+            val cluster = dimensions["cluster"]
+            val brand = dimensions["brand"]
+
+            val allowedBrands = allowedCombinations[cluster]
+
+            variantBuilder.enable = allowedBrands?.contains(brand) ?: false
         }
     }
 }
